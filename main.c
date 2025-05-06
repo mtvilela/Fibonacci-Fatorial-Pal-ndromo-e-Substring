f297bb6cb108ff72e8d12239585dfd196bc7feb4
dudaaquila <dudaaquilaamaral@gmail.com>
2025-05-06 12:11:03 -0300

    Update main.c - melhorando o visual, e deixando mais funcional com looping

--- main.c
+++ main.c
@@ -4,58 +4,66 @@
 
 void fibonacci() {
     int n;
-    printf("Digite a quantidade de termos da sequência de fibonacci (1 a 50): ");
-    scanf("%d", &n);
-    
-    long long fib[50] = {0, 1};
-    
-    if (n < 1 || n > 50) {
-        printf("O número que foi digitado não é válido\n\n");
-        return;
-    }
-    
-    for (int i = 2; i < n; i++) {
-        fib[i] = fib[i-1] + fib[i-2];
+    do {
+        printf("Digite a quantidade de termos da sequência de Fibonacci (1 a 50): ");
+        scanf("%d", &n);
+
+        if (n < 1 || n > 50) {
+            printf("O número digitado não é válido. Tente novamente.\n\n");
         }
-     
-     printf("Sequência de Fibonacci com %d numeros:\n", n);
-    
+    } while (n < 1 || n > 50);
+
+    long long fib[50] = {0, 1};
+
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    printf("Sequência de Fibonacci com %d números:\n", n);
     for (int i = 0; i < n; i++) {
-        printf("%lld \n", fib[i]);
+        printf("%lld\n", fib[i]);
     }
-    printf("\n");}
-    
-void fatoriais() {
-    int n;
-    printf("Digite um numero inteiro (1 - 20) :");
-    scanf("%d", &n);
-    
-    if (n < 1 || n > 20){
-        printf("O número que foi digitado não é válido\n\n");
-        return;
-    }
-    
-    long long fat[20] = {1};
-    
-   for (int i = 1; i < n; i++) {
-    fat[i] = fat[i-1] * (i+1);
+    printf("\n");
 }
 
-    printf("Resultado Fatoriais até %d\n", n);
+void fatoriais() {
+    int n;
+    do {
+        printf("Digite um número inteiro (1 a 20): ");
+        scanf("%d", &n);
+
+        if (n < 1 || n > 20) {
+            printf("O número digitado não é válido. Tente novamente.\n\n");
+        }
+    } while (n < 1 || n > 20);
+
+    long long fat[20] = {1};
+
+    for (int i = 1; i < n; i++) {
+        fat[i] = fat[i - 1] * (i + 1);
+    }
+
+    printf("Resultado dos fatoriais até %d:\n", n);
     for (int i = 0; i < n; i++) {
-    printf("%d! = %lld\n", i+1, fat[i]);
-}
+        printf("%d! = %lld\n", i + 1, fat[i]);
+    }
     printf("\n");
 }
 
 int main() {
     int escolha;
-    
+
     while (1) {
-        printf("Digite o numero do exercicio que deseja selecionar\n1: Fibonacci\n2: Fatoriais\n0: Sair\n=");
-        scanf("%i", &escolha);
-        
-        switch(escolha) {
+        printf("========== MENU ==========\n");
+        printf("1: Fibonacci\n");
+        printf("2: Fatoriais\n");
+        printf("0: Sair\n");
+        printf("==========================\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &escolha);
+        printf("\n");
+
+        switch (escolha) {
             case 1:
                 fibonacci();
                 break;
@@ -63,8 +71,11 @@ int main() {
                 fatoriais();
                 break;
             case 0:
-                printf("O programa será encerrado\n");
+                printf("O programa será encerrado.\n");
                 return 0;
+            default:
+                printf("Opção inválida. Por favor, escolha 0, 1 ou 2.\n\n");
+                break;
         }
     }
 }
